431277e7eccaa2a5289f1e8d1ba3d4a36208ac42
Filipe <filipeasm18@gmail.com>
2022-08-04 21:00:47 -0300

    Feat: Pointers basic knowledge

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+void pointers() {
+    int var = 20;
+    int *p = &var;
+
+    std::cout << "Value of var: " << var << std::endl;
+    std::cout << "Addres on pointer p: " << p << std::endl;
+    std::cout << "Value of address on pointer p: " << *p << std::endl;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    pointers();
     return 0;
 }
