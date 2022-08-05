6b16c62e31603e2f7e59cc5de863f3b074e74f39
Filipe <filipeasm18@gmail.com>
2022-08-04 23:09:00 -0300

    Feat: add menu to choose theme

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include "pointers/pointers.h"
+#include "references/references.h"
+#include "iostream"
+
+using namespace std;
 
 int main() {
-    menu();
+    int option = 0;
+
+    while (option != -1) {
+        cout << endl;
+        cout << "Choose a theme to learn:" << endl;
+        cout << " 1 - Pointers" << endl;
+        cout << " 2 - References" << endl;
+        cout << "-1 - Exit" << endl;
+        cin >> option;
+
+        switch (option) {
+            case 1:
+                pointersMenu();
+                break;
+
+            case 2:
+                referencesMenu();
+                break;
+
+            case -1:
+                break;
+
+            default:
+                cout << "Invalid operation. Try again." << endl;
+        }
+    }
+
     return 0;
 }
